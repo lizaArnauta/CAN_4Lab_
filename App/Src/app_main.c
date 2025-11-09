db41b8409a5777b6c8e87683e02cdb62ff23dad3
Teodor <teodor.muzychuk@ucu.edu.ua>
2025-11-09 19:24:03 +0200

    feat: blink

--- App/Src/app_main.c
+++ App/Src/app_main.c
@@ -1,7 +1,14 @@
 #include "app_main.h"
+#include "main.h"
+#include "stm32f1xx_hal.h"
 
 void app_main() {
   // main
+
+  while (1) {
+    HAL_GPIO_TogglePin(LED_PIN_GPIO_Port, LED_PIN_Pin);
+    HAL_Delay(500);
+  }
 
   return;
 }
